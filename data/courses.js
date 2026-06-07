bfdbe57a68f83b15fd2e57648a614fdf13ceef40
Aaron Valera <aaronrvalera@gmail.com>
2026-06-07 16:10:03 -0400

    Add courses' information

---
+++ data/courses.js
@@ -0,0 +1,104 @@
+let coursesInfo = {
+    programming: [
+        {
+            id: 1,
+            title: "Learn Python",
+            language: "python",
+            views: 105014,
+            level: "beginner"
+        },
+        {
+            id: 2,
+            title: "Learn Python pt.2",
+            language: "python",
+            views: 9455895,
+            level: "intermediate"
+        },
+        {
+            id: 3,
+            title: "Learn Python pt.3",
+            language: "python",
+            views: 75594,
+            level: "advanced"
+        },
+        {
+            id: 4,
+            title: "Learn JavaScript",
+            language: "javascript",
+            views: 49494,
+            level: "intermediate"
+        },
+        {
+            id: 5,
+            title: "Learn JavaScript pt.2",
+            language: "javascript",
+            views: 39942,
+            level: "advanced"
+        },
+        {
+            id: 6,
+            title: "Learn CSS",
+            language: "css",
+            views: 6050332,
+            level: "intermediate"
+        }
+    ],
+    mathematics: [
+        {
+            id: 1,
+            title: "Learn Algebra",
+            subject: "algebra",
+            views: 2394,
+            level: "beginner"
+        },
+        {
+            id: 2,
+            title: "Learn Algebra pt.2",
+            subject: "algebra",
+            views: 12098,
+            level: "advanced"
+        },
+        {
+            id: 3,
+            title: "Learn Calculus",
+            subject: "calculus",
+            views: 78583,
+            level: "beginner"
+        },
+        {
+            id: 4,
+            title: "Learn Calculus pt.2",
+            subject: "calculus",
+            views: 938,
+            level: "intermediate"
+        },
+        {
+            id: 5,
+            title: "Learn Calculus pt.3",
+            subject: "calculus",
+            views: 9802,
+            level: "advanced"
+        },
+        {
+            id: 6,
+            title: "Learn Geometry",
+            subject: "geometry",
+            views: 8129,
+            level: "intermediate"
+        },
+        {
+            id: 7,
+            title: "Learn Geometry pt.2",
+            subject: "geometry",
+            views: 8410003,
+            level: "advanced"
+        },
+        {
+            id: 8,
+            title: "Learn Statistics",
+            subject: "statistics",
+            views: 44821,
+            level: "intermediate"
+        }
+    ]
+}
